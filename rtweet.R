059cac88680751229fd944c9d8340e607bd5d83a
Moaziz <moaziz130188@gmail.com>
2022-08-23 14:35:03 +1000

    electric vehicle tweets 23 Aug 2022

--- rtweet.R
+++ rtweet.R
@@ -7,21 +7,21 @@ library('tidyverse')
 #keywords from Twitter platform.
 
 target_tweets<-search_tweets(
-  q="smartgrid OR sustainability",
+  q="#electricvehicle OR #EV",
   n=17900,
   include_rts = FALSE,
   lang="en",
   retryonratelimit = TRUE )
 
 #save the retrieved data 'target_tweets' in the current directory.
-target_tweets %>% write_csv("raw_target_tweets20Aug2022.csv")
+target_tweets %>% write_csv("raw_EV_23Aug2022.csv")
 
 
 #Converting the data set into a short and cleaner data set as required. 
-manipulated_tweets20Aug2022<-target_tweets%>%
-  select(user_id, screen_name, created_at, text, favorite_count, 
-         quoted_location, favorite_count, hashtags, mentions_user_id, 
+short_EV_23Aug2022<-target_tweets%>%
+  select(user_id, screen_name, created_at, text, source, display_text_width, favorite_count, 
+         quoted_location, favorite_count, is_retweet, retweet_count, hashtags, mentions_user_id, 
          mentions_screen_name, quoted_followers_count)
 
 #save the manipulated data set 'mtarget_tweets20Aug2022' in the current directory.
-manipulated_tweets20Aug2022 %>% write_csv("manipulated_tweets20Aug2022.csv")
+short_EV_23Aug2022 %>% write_csv("short_EV_23Aug2022.csv")
